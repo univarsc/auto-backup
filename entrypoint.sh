857277b8376870043f4d4afe096399970e8bad71
univarSC <75031981+univarsc@users.noreply.github.com>
2021-05-12 14:38:50 +0100

    Added token to remote url

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,14 @@ cd theme-files/
 
 git config --global user.email "bradley.playdon@univarsolutions.com"
 git config --global user.name "univarsc"
-git remote add origin git@github.com:univarsc/USC-MAIN-SITE.git
+
 
 git checkout backup-main-theme
 
 git add .
 
 git commit -m "Auto backup commit"
+
+git remote add origin 'https://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@github.com/univarsc/USC-MAIN-SITE.git'
 
 git push
