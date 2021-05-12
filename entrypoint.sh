dfd481ee6686714e033a2f3fe4458c622c83a000
univarSC <75031981+univarsc@users.noreply.github.com>
2021-05-12 11:49:01 +0100

    Added git add,commit,push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 theme download --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH
+
+cd theme-files/
+
+git add .
+
+git commit -m "Auto backup commit"
+
+git push
