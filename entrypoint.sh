c4a7a2e223d367db152f5db7bf9f16ec485f13d5
univarSC <75031981+univarsc@users.noreply.github.com>
2021-05-12 14:56:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,6 @@ git add .
 
 git commit -m "Auto backup commit"
 
-git remote add origin 'https://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@github.com/univarsc/USC-MAIN-SITE.git'
+git remote add origin https://$USCPASS@github.com/univarsc/USC-MAIN-SITE.git
 
 git push
